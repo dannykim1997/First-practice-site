883c1a8396288daecda63447468beae06cff286f
Daniel Kim <dannykim1997@gmail.com>
2021-02-16 11:47:29 -0800

    differentiate button popup dialouge per icon

---
+++ script.js
@@ -0,0 +1,13 @@
+function addingEventListener() {
+const input = document.getElementById('input');
+
+function clickAlert() {
+  alert('Fox is one of the most versatile characters in the game. Run down opponents with powerful attack power and speed, or be patient and choose to attack when finding an opening.');
+}
+input.addEventListener('click', clickAlert);
+}
+addingEventListener()
+
+
+
+
