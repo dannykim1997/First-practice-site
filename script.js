b303173f086e338beca91b03cf7591085c55d935
Daniel Kim <dannykim1997@gmail.com>
2021-02-16 14:54:51 -0800

    added javascript description popups

--- script.js
+++ script.js
@@ -1,13 +1,38 @@
-function addingEventListener() {
-const input = document.getElementById('input');
+function foxinfo() {
+const input = document.getElementById('foxinput');
 
-function clickAlert() {
+function clickAlertfox() {
   alert('Fox is one of the most versatile characters in the game. Run down opponents with powerful attack power and speed, or be patient and choose to attack when finding an opening.');
 }
-input.addEventListener('click', clickAlert);
+input.addEventListener('click', clickAlertfox);
 }
-addingEventListener()
+foxinfo()
 
+function falcoinfo() {
+    const input = document.getElementById('falcoinput');
 
+function clickAlertfalco() {
+    alert('Falco has lasers which allow him to lock down opponents from a distance and then combo them with his powerful moveset.')
+}
+input.addEventListener('click', clickAlertfalco);
+}
+falcoinfo()
 
+function marthinfo() {
+    const input = document.getElementById('marthinput');
 
+function clickAlertmarth() {
+    alert('Marth has a long range with his grab and sword. He can attack opponents with proper spacing and superior movement.')
+}
+input.addEventListener('click', clickAlertmarth)
+}
+marthinfo()
+
+function falconinfo() {
+    const input = document.getElementById('falconinput')
+function clickAlertfalcon () {
+    alert('Captain Falcon has some of the strongest attacks in the game. With his fast running speed, he is able to chase down opponents wherever they are.')
+}
+input.addEventListener('click', clickAlertfalcon)
+}
+falconinfo()
